6066a7fdcd092106b653bda6813c71508ec0180a
Egill Antonsson <6976127+EgillAntonsson@users.noreply.github.com>
2023-04-29 12:54:53 +0200

    Handle invalid StartingPoints values: Red, Green, Refactor

--- AvatarHealth-Test/HealthTest.cpp
+++ AvatarHealth-Test/HealthTest.cpp
@@ -1,9 +1,18 @@
 #include "pch.h"
 #include "../AvatarHealth/Health.h"
+#include "HealthTest.h"
 
 TEST(Constructor, CurrentPoints_HasStartingValue)
 {
 	auto startingPoints = 12;
 	Health health = Health(startingPoints);
-	EXPECT_EQ(health.GetCurrentPoints(), 12);
+	EXPECT_EQ(health.GetCurrentPoints(), startingPoints);
+}
+
+TEST(Constructor, ThrowsError_WhenStartingPointsIsInvalid)
+{
+	EXPECT_THROW(
+		{
+			Health health = Health(0);
+		}, std::out_of_range);
 }
